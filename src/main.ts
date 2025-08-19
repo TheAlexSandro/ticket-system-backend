57935bba8167248becaa647960571531fee8ccf9
SCP Foundation ID <jevringarmindo@gmail.com>
2025-08-19 17:51:55 +0700

    using vercel for deployment

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
-import { config } from 'dotenv';
-config({ path: '.env' });
+import { config } from "dotenv";
+config({ path: ".env" });
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AuthGuard } from './resources/security/auth.guard';
-import * as cookieParser from 'cookie-parser';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AuthGuard } from "./resources/security/auth.guard";
+import * as cookieParser from "cookie-parser";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as express from "express";
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
   app.useGlobalGuards(new AuthGuard());
   app.use(cookieParser());
   app.enableCors({
     origin: process.env["FRONTEND_URL"]?.split(","),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
     credentials: true,
   });
 
-  await app.listen(8000, () => {
-    console.log("OK");
-  });
+  await app.init();
 }
+
 bootstrap();
+
+export default server;
